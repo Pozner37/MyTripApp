b1eb26345428eb5a08250cb8b718d5597bc49034
Jumpmane <idopozner10@gmail.com>
2024-03-02 18:09:46 +0200

    initial home fragment trial

--- app/src/main/java/com/example/mytrip/FirstFragment.kt
+++ app/src/main/java/com/example/mytrip/FirstFragment.kt
@@ -5,40 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.mytrip.databinding.FragmentFirstBinding
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_first, container, false)
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
+        // Get the LinearLayout where you want to add buttons
+        val buttonContainer = rootView.findViewById<LinearLayout>(R.id.buttonContainer)
 
+        // Create buttons dynamically and add them to the layout
+        for (i in 1..10) {
+            val imageButton = ImageButton(requireContext())
+            imageButton.setImageResource(R.drawable.flag_of_israel)
+            val sizeInDp = 100
+            val density = resources.displayMetrics.density
+            val sizeInPixels = (sizeInDp * density).toInt()
+
+            val layoutParams = ViewGroup.LayoutParams(sizeInPixels, sizeInPixels)
+            imageButton.background = null
+            imageButton.scaleType = ImageView.ScaleType.CENTER_INSIDE
+            imageButton.layoutParams = layoutParams
+            imageButton.setOnClickListener {
+                Toast.makeText(context, "button works", Toast.LENGTH_SHORT).show()
+            }
+
+            // Add the button to the container
+            buttonContainer.addView(imageButton)
+        }
+
+        return rootView
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+
     }
 }
