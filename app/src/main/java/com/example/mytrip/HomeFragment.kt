c1164d5b3f9596f3a02310cdade9375acfa20e28
Pozner <idopozner01@gmail.com>
2024-03-11 00:44:31 +0200

    added names to the home page country buttons

--- app/src/main/java/com/example/mytrip/HomeFragment.kt
+++ app/src/main/java/com/example/mytrip/HomeFragment.kt
@@ -6,10 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.GridLayout
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import com.example.mytrip.utils.CountriesApiManager
@@ -37,28 +39,31 @@ class HomeFragment : Fragment() {
                 val countries =  countriesApi.getAllCountries()
 
                 for (country in countries) {
-                    val imageButton = ImageButton(requireContext())
-                    val sizeInDp = 100
-                    val density = resources.displayMetrics.density
-                    val sizeInPixels = (sizeInDp * density).toInt()
+                    val buttonLayout = LayoutInflater.from(requireContext()).inflate(R.layout.grid_item_button_with_title, null)
+                    buttonLayout.layoutParams = GridLayout.LayoutParams().apply {
+                width = 0 // Set width to 0 to allow GridLayout to control it
+                height = GridLayout.LayoutParams.WRAP_CONTENT
+                rowSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f) // Adjust weight as needed
+                columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1, 1f) // Adjust weight as needed
+            }
+                    val imageButton: ImageButton = buttonLayout.findViewById(R.id.gridButton)
+                    val imageButtonTitle: TextView = buttonLayout.findViewById(R.id.gridButtonTitle)
 
-                    val layoutParams = ViewGroup.LayoutParams(sizeInPixels, sizeInPixels)
-                    imageButton.background = null
-                    imageButton.scaleType = ImageView.ScaleType.CENTER_INSIDE
-                    imageButton.layoutParams = layoutParams
                     imageButton.setOnClickListener {
                         Toast.makeText(context, country.name.common, Toast.LENGTH_SHORT).show()
                     }
 
-                    val imageUrl = country.flags.png
+                    Picasso.get().load(country.flags.png).into(imageButton)
 
-                    Picasso.get().load(imageUrl).into(imageButton)
+                    imageButtonTitle.text = country.name.common
+
+                   (buttonLayout.parent as? ViewGroup)?.removeView(buttonLayout)
 
                     // Add the button to the container
-                    buttonContainer.addView(imageButton)
+                    buttonContainer.addView(buttonLayout)
                 }
             } catch (e: Exception) {
-                Log.e("YourFragment", "Error fetching data: ${e.message}")
+                Log.e("HomePage", "Error while creating home page: ${e.message}")
             }
         }
 
